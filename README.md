b4e9c7d0f75874d69f8f883a2f812ccfe64d6a8e
adammccartney <adam@mur.at>
2021-05-28 14:39:13 +0200

    updated readme

--- README.md
+++ README.md
@@ -6,4 +6,13 @@ Examples in this repository center around two texts:
 + The Linux Programming Interface by Michael Kerrisk
 
 The programs contained in this repo are copies of ones found in the books and
-some exercises around similar thematic areas. 
+some exercises on similar topics. 
+
+## .PHONY: compare
+Make is used throughout the repository to build small example programs. 
+In some cases, it's interesting and informative to view the assembly output by
+the compiler with different optimization flags. In such examples, there will
+often be a phony compare target that builds both versions and opens up a diff
+in vim.
+
+
--- process_api/callingfork/callingfork.c
+++ process_api/callingfork/callingfork.c
@@ -1,4 +1,4 @@
-/* callingfork.c: from ostep section on process_api
+/* callingfork.c: solution to homework question 1 chpt.6 ostep
  * here we see that a call to fork produces indeterminate behavior
  * */
 
@@ -9,16 +9,18 @@
 
 int main(int argc, char *argv[])
 {
-    printf("Hello world (pid:%d)\n", (int) getpid());
+    int x = 100; 
     int rc = fork();
     if (rc < 0) { // fork failed...fork it!
         fprintf(stderr, "fork failed\n");
         exit(1);
     } else if (rc == 0) { // child (new process)
-        printf("hello, I am child (pid:%d)\n", (int) getpid());
+        x = 900; /* change val of x in child */
+        printf("(child) x: %d (pid:%d)\n", x, (int) getpid());
     } else {
-        printf("hello, I am parent of %d (pid:%d)\n",
-                rc, (int) getpid());
+        x = 700; /* change val of x in parent */
+        printf("(parent %d) x: %d (pid:%d)\n",
+                rc, x, (int) getpid());
     }
     return 0;
 }
