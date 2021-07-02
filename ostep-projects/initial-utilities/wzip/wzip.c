d5ab86aeb07abc9b32fb22c9837a6585d97b6d5a
adammccartney <adam@mur.at>
2021-07-02 15:41:05 +0200

    program complete, tests passing. Learned something about off by one errors that i will never forget

--- ostep-projects/initial-utilities/wzip/wzip.c
+++ ostep-projects/initial-utilities/wzip/wzip.c
@@ -24,16 +24,14 @@ size_t numbytes (struct token t) {
     return mybytes;
 }
 
-int printToken (struct token t) { 
+void printToken (struct token t) { 
     /* write <token.count><token.ch> to stdout */
     /* write number of occurrences of char as 4 byte int,
      * followed by char as ASCII i
      * function returns 0 on success, 1 on error
      * */
-    
     if ((t.ch < 0) || (t.ch > 127)) {
         perror("Error: token not a valid ascii character\n");
-        return 1; /* shiznit ... */
     }
 
     size_t bytecheck;
@@ -43,21 +41,17 @@ int printToken (struct token t) {
         printf("%c", t.ch);
     } else {
         perror("Error: compression format output not equal to five bytes\n");
-        return 1; /* also shiznit ... */
     }
-    return 0; /* print successful */
 }
 
 struct token updateTokenCount (struct token t, char flag) {
     /* updates token.count */
     if (flag == 's') { /* flag (s)ustain, assumes token.ch is sustained */
        t.count++;
-    } else if (flag == 'n') { /* flag (n)ew, assumes token.ch is new */ 
-                              /* this assumption is made on having seen */
-                              /* that the next char is a new token */
-        t.count = 0;          /* resets t.count to initial value */
-    } else if (flag == 'i') { /* we've met the initial token */
+    } else if (flag == 'i') { /* initialize count */
         t.count = 1;
+    } else if (flag == 'n') { /* set to zero */
+        t.count = 0;
     } else {
         perror("Error: uknown flag passed to updateTokenCount");
     }
@@ -70,83 +64,95 @@ struct token updateTokenCh (struct token t, u_int8_t ch) {
     return t;
 }
 
-bool isatend (ssize_t lsz, int current) { 
-    if (current >= lsz) {
+
+bool isAtEnd (ssize_t lsz, int current) { 
+    if (current + 1 >= lsz - 1) { /* don't count null char at end of buf */
         return true;
     }
     return false;
 }
 
-
+bool isRepeat(struct token t, u_int8_t initchar) {
+    if (t.ch == initchar) {
+        return true;
+    }
+    return false;
+}
 
 bool isFirstOrigChar (int current, char lstch, struct token t) {
-    /* Assumes that the token may be carrying state from another file
+    /* Assumes that the token may be carrying state from another line  
      * so it performs three checks: */
     if ((current == 0)    &&     /* is head at initial read position */
-        (lstch == '\0')   &&     /* is the lstch set to NULL */
-                                 /* last char may not be NULL if the last file 
-                                  * has state to maintain */
-        (t.count == 0)) {        /* is the token count zero */
+        (t.count == 0)    &&     /* is the token count zero */
+        (t.ch == '\0') ) {       /* is the token.ch at initial state */  
         return true;
     }
     return false;
 }
 
-bool isNormRepeatChar (bool endpos, u_int8_t cur, u_int8_t nxt, int current) {
-    /* Assumes we might be about to overflow the buffer or that next 
-     * char is new, checks for: */
-    if ((endpos == false) &&  /* end of buffer */ 
-        (cur == nxt)      &&  /* equality */
-        (current != 0)) {     /* not initial char */
+bool isFirstCharRepeat (int current, char lstch, struct token t) {
+    /* Assumes that we've received state from a previous line */
+    if ((current == 0) &&
+        (t.count > 0) &&
+        (t.ch > '\0')) {
+        return true;
+    }
+    return false;
+}
+
+bool isNewChar (int current, u_int8_t curch, u_int8_t lstch, struct token t) {
+    /* Assumes that the token may be carrying state from another file
+     * so it performs three checks: */
+    if ((curch != t.ch)   &&
+        (curch != '\0')    &&
+        (current != 0)    &&     /* is head at initial read position */
+        (lstch != '\0'))   {     /* is the lstch set to NULL */
+        return true;
+    }
+    return false;
+}
+
+bool isLineRepeatChar (u_int8_t curch, u_int8_t lstch) {
+    if (curch == lstch) {
+        return true;
+    }
+    return false;
+}
+
+bool isRepeatChar (int current, u_int8_t curch, u_int8_t lstch) {
+    /* Assumes that cur and nxt are equal 
+     * we're not at the first read position in line */
+    if ((curch == lstch)   &&  /* equality */
+        (curch != '\0')   &&
+        (current != 0)    &&    /* not initial char */
+        (lstch != '\0')) {
         return true; 
     }
     return false;
 }
 
-bool isNewCharInLine (bool endpos, u_int8_t cur, u_int8_t nxt) {
-    /* Assumes that we might overflow the buffer 
-     * or next char repeats checks: */
-    if ((endpos == false) &&    /* will not try to read invalid */
-        (cur != nxt)) {         /* different chars */
+bool isEndOfLine (u_int8_t cur, u_int8_t nxt) {
+    /* assumes that the following conditions 
+     * for end of line:   */
+    if ((cur != nxt) &&  /* characters are inequal */ 
+        (nxt == '\0')) { /* the next char reads null */
         return true;
     }
     return false;
 }
 
-bool isEndOfLine (bool endpos, u_int8_t cur, u_int8_t nxt) {
-    if ((cur != nxt) &&
-        (endpos == true)) {
-        return true;
-    }
-    return false;
-}
 
-bool isLineRepeatChar (bool endpos, int current, char lstch) {
-    /* Assume that we have received a token with state from the last line 
-     * perform three checks : */
-    if ((endpos == false) && /* check not overflowing buffer */
-        (current == 0)    && /* head is at init read position */
-        (lstch == '\0')) {   /* last char was not null */
-        return true;
-    }
-    return false;
-}
-
-u_int8_t advance (const char *line, ssize_t lsz, int current) {
-    u_int8_t tok = '\0';
-    if (current < lsz) {
-        tok = line[current];
-    }
-    return tok; /* returns null if current overflows buffer */
+u_int8_t peek (const char *line, int current) {
+    /* current is the index currently being read */
+    /* line size */
+    return line[current];
 }
 
 u_int8_t peeknext (const char *line, ssize_t lsz, int current) {
-    u_int8_t tok = '\0'; /* initialize on the empty ch 
-                          * returns null if next overflows buffer */
-    if ((current + 1) < lsz) {
-        tok = line[current];
+    if (isAtEnd(lsz, current)) { 
+        return '\0';
     }
-    return tok;
+    return line[current + 1];
 }
 
 /* The following is basically a scanner
@@ -161,71 +167,45 @@ struct token readlines (FILE *fp, struct token t)
     size_t len = 0;         /* size of buffer */
     ssize_t linelen = 0;    /* number of chars read into buffer */
     int current;            /* used by scanner to indicate current read pos */
-    int succ = 1;           /* failbit ... or fail 4 bytes ... whatever ! */
     u_int8_t initchar = '\0'; /* initial character */
     u_int8_t curch = '\0';  /* current char */
-    u_int8_t nxtch = '\0';  /* next char */
     u_int8_t lstch = '\0';
-    bool endpos = false;
 
     while ((linelen = getline(&line, &len, fp)) != -1) {
         
-        int j;
-        current = 0;  /* set current position to beginning of line */
-        initchar = line[current];   /* read lines first character */
-
-        if ((lstch != initchar) && 
-            (lstch != '\0')) { /* new line, new char */
-            succ = printToken(t);
-            printf("%c", '\n'); /* print control character null */
-        } 
-
-        for (j = 0; j < linelen; j++) { 
-            
-            curch = advance(line, linelen, current); /* advancing the curch of nameology*/
-            nxtch = peeknext(line, linelen, current);
-            endpos = isatend(linelen, current);
-            //printf("t.count: %d\n", t.count);
-
-            if (nxtch == '\0') { /* end of line attempt at buffer overflow */
-                //printf("control 0\n");
-                endpos = true;
+        current = 0;
+        initchar = line[current];
+        /* if there is state from last line, print it */
+        if (lstch != '\0') {
+            if (!(isLineRepeatChar(lstch, initchar))) {
+                  printToken(t);
+                  t = updateTokenCount(t, 'n');
+                  t = updateTokenCh(t, '\0');
             }
+        }
+        for (; current < linelen; current++) {  
+            curch = peek(line, current); 
+
+            /* the order of these statements is important */ 
+            if (isFirstCharRepeat(current, lstch, t)) {
+                t = updateTokenCount(t, 's');
+            }       
 
             if (isFirstOrigChar(current, lstch, t)) {
-                //printf("control 1\n");
                 t = updateTokenCount(t, 'i');  
                 t = updateTokenCh(t, curch);
             }
-
-            if (isNormRepeatChar(endpos, curch, nxtch, current)) {                
-                //printf("control 2\n");
-                t = updateTokenCount(t, 's');
+             
+            if (isNewChar(current, curch, lstch, t)) {
+                printToken(t);
+                t = updateTokenCount(t, 'i');
                 t = updateTokenCh(t, curch);
             }
-            
-            if (isNewCharInLine(endpos, curch, nxtch)) { /* new char within bounds */
-                /* assumes that nxt value will be new char
-                 * so write current state and reset buffer counter */
-                //printf("control 3\n");
-                succ = printToken(t);  /* if nxt is new char */
-                t = updateTokenCount(t, 'n');
-                /* does **not** take action to update token.ch */
-                /* this happens in next iteration */
-            } 
 
-            if (isLineRepeatChar(endpos, current, lstch)) {  
-                /* char carries over from last line */
-                //printf("control 4\n");
+            if (isRepeatChar(current, curch, lstch)) {
                 t = updateTokenCount(t, 's');
             }
-            
-            if (isEndOfLine(endpos, curch, nxtch)) { /* end of line, token has state */
-                //printf("control 5\n");
-                lstch = curch;      /* control for next line */
-            }
-            current++;
-            //printf("current count: %d\n", current);
+            lstch = curch; 
         }            
     }
     free(line);
@@ -250,7 +230,7 @@ int procfiles (int nfiles, char *files[]) {
             i++; /* get next file */
         }
         if ( i >= nfiles ) { /* at last file, flush state */
-            succ = printToken(curtok);
+            printToken(curtok);
             curtok = updateTokenCount(curtok, 'n');  /* next token is NULL */ 
             curtok = updateTokenCh(curtok, '\0'); /* end state */
         }
